e48af7b0978a1dbe75922a3a5f64575516d1f9e2
Kelly Cook <kellyrcook1980@gmail.com>
2021-09-12 12:20:16 -0600

    apollo set up, typeDefs and resolvers working

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
-// set up express and db
+// set up express and db and apollo
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 // set up port
 const PORT = process.env.PORT || 3001;
 
-// set up express app
+// set up express app and use middleware for apollo
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
 const app = express();
+server.applyMiddleware({app});
+// server.start().then(() => {
+//     server.applyMiddleware({ app });
+//   });
+
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// open server port
+//open server port
+
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server running on port localhost:${PORT}!`);
+    console.log(`API server running on port http://localhost:${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
