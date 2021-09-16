0339aaabc06e178515bcdf0ebae7c9ef4a256f24
Kelly Cook <kellyrcook1980@gmail.com>
2021-09-16 15:33:29 -0600

    add friend, thoughts, and reactions in resolvers

--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -35,8 +35,11 @@ const typeDefs = gql`
         username: String
     }, 
     type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth,
+        addUser(username: String!, email: String!, password: String!): Auth,
+        addThought(thoughtText: String!): Thought,
+        addReaction(thoughtId: ID!, reactionBody: String!): Thought,
+        addFriend(friendId: ID!): User
     }
 `;
 
